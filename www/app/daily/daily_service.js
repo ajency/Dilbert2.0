7a5ca2a0ebf8aa1d73a7e5382eb368432fe8ad82
Clinton D'souza <clinton@ajency.in>
2015-02-26 11:23:55 +0530

    no message

--- www/app/daily/daily_service.js
+++ www/app/daily/daily_service.js
@@ -13,9 +13,31 @@ angular.module('daily.service', []).service('DailyService', [
           task: 'Dilbert Mobile - Object Defination',
           status: 'available',
           endtime: 1422528000
+        }, {
+          task: 'Dilbert Mobile - WireFrame Design',
+          status: 'available',
+          endtime: 1422534600
+        }, {
+          task: 'Dilbert Mobile - WireFrame Design',
+          status: 'available',
+          endtime: 1422538200
+        }, {
+          task: 'Dilbert Mobile - Static Layout',
+          status: 'available',
+          endtime: 1422541800
+        }, {
+          task: 'Dilbert Mobile - Static Layout',
+          status: 'available',
+          endtime: 1422546400
+        }, {
+          task: 'Dilbert Mobile - Bug Fixes',
+          status: 'available',
+          endtime: 1422552600
         }
       ]
     ];
-    return this.getDailyData = function() {};
+    return this.getDailyData = function() {
+      return $scope.data;
+    };
   }
 ]);
--- www/source/daily/daily_service.coffee
+++ www/source/daily/daily_service.coffee
@@ -12,11 +12,36 @@ angular.module('daily.service',[])
 				task:'Dilbert Mobile - Object Defination'
 				status:'available'
 				endtime:1422528000
+			}
+			{
+				task:'Dilbert Mobile - WireFrame Design'
+				status:'available'
+				endtime:1422534600
+			}
+			{
+				task:'Dilbert Mobile - WireFrame Design'
+				status:'available'
+				endtime:1422538200
+			}
+			{
+				task:'Dilbert Mobile - Static Layout'
+				status:'available'
+				endtime:1422541800
+			}
+			{
+				task:'Dilbert Mobile - Static Layout'
+				status:'available'
+				endtime:1422546400
+			}
+			{
+				task:'Dilbert Mobile - Bug Fixes'
+				status:'available'
+				endtime:1422552600
 			}			
 		]
 
 	]
 	@.getDailyData=()->
-
+		$scope.data
 
 ]
